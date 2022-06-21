2ac2c84f0ac8aee55b551e787e3360bc5028c80d
hyung000620 <dksms1@naver.com>
2022-06-21 17:03:36 +0900

    solar 웹뷰

---
+++ SOLAR/webview.php
@@ -0,0 +1,36 @@
+<?
+$HEADER_INFO = apache_request_headers();
+
+//print_r( $headers );
+//echo $HEADER_INFO["CALL-FROM"];
+//echo $HEADER_INFO["CALL-TIME"];
+$CALL_FROM=$HEADER_INFO["CALL-FROM"];
+if(!$CALL_FROM) $CALL_FROM="NONE";
+
+$bgn_stmp=strtotime("-5 minutes");
+$end_stmp=strtotime("+5 minutes");
+$sol_stmp=$HEADER_INFO["CALL-TIME"];
+
+if($sol_stmp > $bgn_stmp && $sol_stmp < $end_stmp) $AUTH="YES"; 
+else $AUTH="NO";
+
+define("CALL_FROM",$CALL_FROM);
+define("SOLAR_AUTH",$AUTH);
+
+//echo CALL_FROM;
+//echo SOLAR_AUTH;
+$url=urldecode($_GET['url']);
+$_path=parse_url($url,PHP_URL_PATH);
+$_query=parse_url($url,PHP_URL_QUERY);
+$_queryArr=explode("&", $_query);
+foreach($_queryArr as $q)
+{
+	list($k,$v)=explode("=",$q);
+	${$k}=$v;
+}
+
+$ref_from="solar";
+$client_id="solar";
+
+include $_SERVER["DOCUMENT_ROOT"].$_path;
+?>
